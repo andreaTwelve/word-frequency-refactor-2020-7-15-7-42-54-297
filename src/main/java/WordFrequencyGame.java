719fc8fba14657c400b13332d812100fd41c1800
andrea <andreatwelve@sina.cn>
2020-07-27 22:10:20 +0800

    [word-frequency] [andrea] refactor: format the code

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -7,28 +7,22 @@ public class WordFrequencyGame {
     private static final String CALCULATE_ERROR = "Calculate Error";
 
     public String getResult(String sentence) {
+        try {
+            List<WordInfo> wordInfos = calculateWordFrequency(sentence);
 
+            wordInfos.sort((wordInfo1, wordInfo2) -> wordInfo2.getWordCount() - wordInfo1.getWordCount());
 
-
-
-            try {
-
-
-                List<WordInfo> wordInfos = calculateWordFrequency(sentence);
-
-                wordInfos.sort((wordInfo1, wordInfo2) -> wordInfo2.getWordCount() - wordInfo1.getWordCount());
-
-                return getWordFrequencyResult(wordInfos);
-            } catch (Exception e) {
-                return CALCULATE_ERROR;
-            }
+            return getWordFrequencyResult(wordInfos);
+        } catch (Exception e) {
+            return CALCULATE_ERROR;
+        }
 
     }
 
     private List<WordInfo> calculateWordFrequency(String sentence) {
         List<String> words = Arrays.asList(sentence.split(BLANK_SPACE));
         List<WordInfo> wordInfos = new ArrayList<>();
-        for (String uniqueWord: new HashSet<>(words)) {
+        for (String uniqueWord : new HashSet<>(words)) {
             wordInfos.add(new WordInfo(uniqueWord, Collections.frequency(words, uniqueWord)));
         }
         return wordInfos;
@@ -36,14 +30,12 @@ public class WordFrequencyGame {
 
     private String getWordFrequencyResult(List<WordInfo> wordInfos) {
         StringJoiner joiner = new StringJoiner(NEW_LINE);
-        for (WordInfo wordInfo: wordInfos) {
-            String wordFrequencyResult = wordInfo.getValue() + " " +wordInfo.getWordCount();
+        for (WordInfo wordInfo : wordInfos) {
+            String wordFrequencyResult = wordInfo.getValue() + " " + wordInfo.getWordCount();
             joiner.add(wordFrequencyResult);
         }
         return joiner.toString();
     }
-
-
 
 
 }
